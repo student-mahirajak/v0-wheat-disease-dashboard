8db6a5908db71b6ce7a9978460c52baa1cce74a3
Mahi Rajak <mahi@email.com>
2026-04-14 16:15:06 +0530

    backend added

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    return {"prediction": "Leaf Rust"}
